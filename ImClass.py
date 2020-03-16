b416e4c0cd54e70afe9f2f5b3c02b696b080c523
Joey Wilson <jwilsoniv@me.com>
2020-03-16 12:55:27 -0700

    Separated LeNet into a different class

--- ImClass.py
+++ ImClass.py
@@ -1,3 +1,4 @@
+from LeNet import LeNet
 from tensorflow.python.ops import variables
 from tensorflow.python.framework import ops
 import pickle
@@ -48,53 +49,10 @@ if __name__ == "__main__":
     test_y = test_labels
 
     # Make graph
-    tf.reset_default_graph()
-    graph = tf.Graph()
-    with graph.as_default():
-        X = tf.placeholder(tf.float32, (None, 32, 32, 3))
-        labels = tf.placeholder(tf.int32, (None,))
-        # filter shape: filter_height, filter_width, in_channels, out_channels
-        with tf.name_scope("conv1"):
-            filters1 = tf.get_variable('filters1', shape=(5, 5, 3, 6), initializer=tf.keras.initializers.glorot_normal())
-            bias1 = tf.get_variable('bias1', shape=(6,), initializer=tf.zeros_initializer())
-            X1 = tf.nn.conv2d(X, filters1, strides=1, padding="VALID")
-            X1 = tf.nn.bias_add(X1, bias1)
-            X1 = tf.nn.leaky_relu(X1)
-            X1 = tf.nn.avg_pool2d(X1, ksize=2, strides=2, padding="VALID")
-        with tf.name_scope("conv2"):
-            filters2 = tf.get_variable('filters2', shape=(5, 5, 6, 16), initializer=tf.keras.initializers.glorot_normal())
-            bias2 = tf.get_variable('bias2', shape=(16,), initializer=tf.zeros_initializer())
-            X2 = tf.nn.conv2d(X1, filters2, strides=1, padding="VALID")
-            X2 = tf.nn.bias_add(X2, bias2)
-            X2 = tf.nn.leaky_relu(X2)
-            X2 = tf.nn.avg_pool2d(X2, ksize=2, strides=2, padding="VALID")
-            X2 = tf.layers.flatten(X2)
-        with tf.name_scope("fcn1"):
-            W1 = tf.get_variable('W1', shape=(400, 120), initializer=tf.keras.initializers.glorot_normal())
-            b1 = tf.Variable(tf.zeros((120,)), trainable=True)
-            X3 = tf.nn.leaky_relu(tf.matmul(X2, W1) + b1)
-        with tf.name_scope("fcn2"):
-            W2 = tf.get_variable('W2', shape=(120, 84), initializer=tf.keras.initializers.glorot_normal())
-            b2 = tf.Variable(tf.zeros((84,)), trainable=True)
-            X4 = tf.nn.leaky_relu(tf.matmul(X3, W2) + b2)
-        with tf.name_scope("softmax"):
-            W3 = tf.get_variable('W3', shape=(84, 10), initializer=tf.keras.initializers.glorot_normal())
-            b3 = tf.Variable(tf.zeros((10,)), trainable=True)
-            X5 = tf.add(tf.matmul(X4, W3), b3)
-            if training:
-                X5 = tf.nn.dropout(X5, rate=0.2)
+    LeNet = LeNet(LEARNING_RATE)
 
-        predictions = tf.nn.softmax(X5)
-
-        cost = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=predictions, labels=labels)
-        loss = tf.reduce_mean(cost)
-
-        optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE)
-        train_op = optimizer.minimize(loss)
-        init = tf.global_variables_initializer()
-
-    with tf.Session(graph=graph) as sess:
-        init.run()
+    with tf.Session(graph=LeNet.graph) as sess:
+        LeNet.init.run()
         step = 0
         for epoch in range(NUM_EPOCHS):
             pos = 0
@@ -106,7 +64,7 @@ if __name__ == "__main__":
                 batch_indices = rand_indices[pos:pos+BATCH_SIZE]
                 batch_x = train_data[batch_indices]
                 batch_y = [train_labels[i] for i in batch_indices]
-                feed_dict = {X: batch_x, labels: batch_y}
+                feed_dict = {LeNet.X: batch_x, LeNet.labels: batch_y}
 
                 if pos == -1:
                     var_list = (variables.trainable_variables() + ops.get_collection(
@@ -115,8 +73,8 @@ if __name__ == "__main__":
                     for v in var_list:
                         print('  ', v.name)
                     # get all gradients
-                    grads_and_vars = optimizer.compute_gradients(loss)
-                    train_op = optimizer.apply_gradients(grads_and_vars)
+                    grads_and_vars = LeNet.optimizer.compute_gradients(LeNet.loss)
+                    train_op = LeNet.optimizer.apply_gradients(grads_and_vars)
 
                     zipped_val = sess.run(grads_and_vars, feed_dict=feed_dict)
 
@@ -127,15 +85,15 @@ if __name__ == "__main__":
                         print('value', rsl[1])
                     exit()
 
-                _, loss_val, outs, labs, W_check = sess.run([train_op, loss, predictions, labels, W3], feed_dict=feed_dict)
+                _, loss_val, outs, labs = sess.run([LeNet.train_op, LeNet.loss, LeNet.predictions, LeNet.labels], feed_dict=feed_dict)
                 avg_loss += loss_val
                 step += 1
                 pos += BATCH_SIZE
 
             # Accuracy on Test Set
             training = False
-            feed_dict = {X: test_x, labels: test_y}
-            test_outs, test_labs = sess.run([predictions, labels], feed_dict=feed_dict)
+            feed_dict = {LeNet.X: test_x, LeNet.labels: test_y}
+            test_outs, test_labs = sess.run([LeNet.predictions, LeNet.labels], feed_dict=feed_dict)
             test_preds = np.argmax(test_outs, axis=1)
             accuracy = 100.0 * np.sum(np.equal(test_labs, test_preds))/len(test_labs)
 
---
+++ LeNet.py
@@ -0,0 +1,63 @@
+from tensorflow.python.ops import variables
+from tensorflow.python.framework import ops
+import pickle
+import pandas as pd
+import numpy as np
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+
+class LeNet:
+    def __init__(self, learning_rate=0.007):
+        tf.reset_default_graph()
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            self.X = tf.placeholder(tf.float32, (None, 32, 32, 3))
+            self.labels = tf.placeholder(tf.int32, (None,))
+        self.training = True
+        self.predictions, self.cost, self.loss, self.train_op, self.init = self.make_graph(learning_rate)
+
+    def make_graph(self, learning_rate):
+        with self.graph.as_default():
+            # filter shape: filter_height, filter_width, in_channels, out_channels
+            with tf.name_scope("conv1"):
+                filters1 = tf.get_variable('filters1', shape=(5, 5, 3, 6),
+                                           initializer=tf.keras.initializers.glorot_normal())
+                bias1 = tf.get_variable('bias1', shape=(6,), initializer=tf.zeros_initializer())
+                X1 = tf.nn.conv2d(self.X, filters1, strides=1, padding="VALID")
+                X1 = tf.nn.bias_add(X1, bias1)
+                X1 = tf.nn.leaky_relu(X1)
+                X1 = tf.nn.avg_pool2d(X1, ksize=2, strides=2, padding="VALID")
+            with tf.name_scope("conv2"):
+                filters2 = tf.get_variable('filters2', shape=(5, 5, 6, 16),
+                                           initializer=tf.keras.initializers.glorot_normal())
+                bias2 = tf.get_variable('bias2', shape=(16,), initializer=tf.zeros_initializer())
+                X2 = tf.nn.conv2d(X1, filters2, strides=1, padding="VALID")
+                X2 = tf.nn.bias_add(X2, bias2)
+                X2 = tf.nn.leaky_relu(X2)
+                X2 = tf.nn.avg_pool2d(X2, ksize=2, strides=2, padding="VALID")
+                X2 = tf.layers.flatten(X2)
+            with tf.name_scope("fcn1"):
+                W1 = tf.get_variable('W1', shape=(400, 120), initializer=tf.keras.initializers.glorot_normal())
+                b1 = tf.Variable(tf.zeros((120,)), trainable=True)
+                X3 = tf.nn.leaky_relu(tf.matmul(X2, W1) + b1)
+            with tf.name_scope("fcn2"):
+                W2 = tf.get_variable('W2', shape=(120, 84), initializer=tf.keras.initializers.glorot_normal())
+                b2 = tf.Variable(tf.zeros((84,)), trainable=True)
+                X4 = tf.nn.leaky_relu(tf.matmul(X3, W2) + b2)
+            with tf.name_scope("softmax"):
+                W3 = tf.get_variable('W3', shape=(84, 10), initializer=tf.keras.initializers.glorot_normal())
+                b3 = tf.Variable(tf.zeros((10,)), trainable=True)
+                X5 = tf.add(tf.matmul(X4, W3), b3)
+                if self.training:
+                    X5 = tf.nn.dropout(X5, rate=0.2)
+
+            predictions = tf.nn.softmax(X5)
+
+            cost = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=predictions, labels=self.labels)
+            loss = tf.reduce_mean(cost)
+
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+            train_op = optimizer.minimize(loss)
+            init = tf.global_variables_initializer()
+        return predictions, cost, loss, train_op, init
