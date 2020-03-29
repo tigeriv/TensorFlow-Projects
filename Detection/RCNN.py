4f74144a37ccba8b2e50e19fefc8d79d9317f0db
Joey Wilson <jwilsoniv@me.com>
2020-03-29 13:22:55 -0700

    Bugs need fixing

--- Detection/RCNN.py
+++ Detection/RCNN.py
@@ -20,7 +20,7 @@ class RCNN:
             self.X = tf.placeholder(tf.float32, (None, shape[0], shape[1], 3))
             self.labels = tf.placeholder(tf.int32, (None, None, 4))
         self.training = True
-        self.init, self.features, self.batch_rects = self.make_graph(learning_rate)
+        self.init, self.features, self.indices = self.make_graph(learning_rate)
         self.batch_size = -1
 
     # Load Feature Encoder
@@ -85,8 +85,36 @@ class RCNN:
             with tf.name_scope("vgg"):
                 features = self.load_vgg()
 
+            with tf.name_scope("rpn"):
+                # Consider each point in 60 x 60 an anchor
+                # For a 3x3 window, there will be 9 anchors
+                # Classifications will then be 60 x 60 x 9
+                filters1 = tf.get_variable('filters1', shape=(3, 3, 512, 9),
+                                           initializer=tf.keras.initializers.glorot_normal())
+                bias1 = tf.get_variable('bias1', shape=(9,), initializer=tf.zeros_initializer())
+                cls = tf.nn.conv2d(features, filters1, strides=1, padding="SAME")
+                cls = tf.nn.bias_add(cls, bias1)
+                cls = tf.nn.sigmoid(cls)
+
+                # Each anchor (9) in point indexed by y1, x1, y2, x2
+                # 60 x 60 x 36
+                filters2 = tf.get_variable('filters2', shape=(3, 3, 512, 36),
+                                           initializer=tf.keras.initializers.glorot_normal())
+                bias2 = tf.get_variable('bias2', shape=(36,), initializer=tf.zeros_initializer())
+                regr = tf.nn.conv2d(features, filters2, strides=1, padding="SAME")
+                regr = tf.nn.bias_add(regr, bias2)
+                regr = tf.nn.leaky_relu(regr)
+                regr = tf.reshape(regr, (60, 60, 9, 4))
+
+            # Calculate ROI
+            # Remove boxes out of bounds (just check x, y)
+            # NMS with 0.7
+            boxes = tf.reshape(regr, (-1, 60*60*9, 4))
+            scores = tf.layers.flatten(cls)
+            selected_indices = tf.image.non_max_suppression(boxes, scores, iou_threshold=0.7, max_output_size=100)
+
             init = tf.global_variables_initializer()
-        return init, features
+        return init, features, selected_indices
 
 
 # Test program
@@ -95,7 +123,10 @@ if __name__ == "__main__":
     waymo_data = WaymoData()
     with tf.Session(graph=rcnn.graph) as sess:
         rcnn.init.run()
-        feed_dict = {rcnn.X: new_batch[0]}
+        new_batch = waymo_data.get_batch()
+        # Batch size 1 for quick testing
+        temp_batch = [new_batch[0][0], new_batch[1][0], new_batch[2][0]]
+        feed_dict = {rcnn.X: temp_batch[0]}
         # Obtain features
-        features, batch_rects = sess.run(rcnn.features, rcnn.batch_rects, feed_dict=feed_dict)
-        print(batch_rects)
+        features, indices = sess.run(rcnn.features, rcnn.indices, feed_dict=feed_dict)
+        print(indices)
